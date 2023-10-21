13d25397e898dd1ec626fc0e1a56d1bd1216c0af
Nans Laupretre <nans.laupretre@gmail.com>
2023-10-21 22:40:49 +0200

    feat(Build): Add terser to remove comments in bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import { terser } from 'rollup-plugin-terser'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  build: {
+    minify: 'terser',
+    rollupOptions: {
+      plugins: [
+        terser({
+          format: {
+            comments: false,
+          },
+
+          mangle: {
+            keep_classnames: false,
+            reserved: [],
+          },
+        }),
+      ],
+    },
+  },
 })
